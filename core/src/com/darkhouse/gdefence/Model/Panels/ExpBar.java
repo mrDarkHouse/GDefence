5d18fd6e2e41baa8ea33705b3b8a78fa4da72b0a
mrdark_house <mrdark_house@rambler.ru>
2016-10-13 12:39:21 +0300

    fixed gold&exp bonus from level #2

--- core/src/com/darkhouse/gdefence/Model/Panels/ExpBar.java
+++ core/src/com/darkhouse/gdefence/Model/Panels/ExpBar.java
@@ -9,17 +9,9 @@ import com.darkhouse.gdefence.Helpers.AssetLoader;
 
 public class ExpBar extends ProgressBar{
     public ExpBar(int width, int height, int x, int y) {
-        super(0, 100, 0.5f, false, AssetLoader.getExpBarSkin());
-
-        //ProgressBar bar = new ProgressBar(0, 10, 0.5f, false, AssetLoader.getExpBarSkin());
-        int expBarSize[] = {width, height};
+        this(width, height);
         setPosition(x, y);
-        //setPosition(Gdx.graphics.getWidth() - expBarSize[0], userlevelButton.getY() - expBarSize[1] - 4);
-        setSize(expBarSize[0], expBarSize[1]);
-        // bar.setAnimateDuration(5);
 
-        //setValue(7.8f);
-        setValue(GDefence.getInstance().user.getCurrentExp());
     }
 
     public ExpBar(int width, int height) {
