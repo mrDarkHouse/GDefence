c3b13b67c76a539a86d7dd7f35d1c27b3b302bbe
mrdark_house <mrdark_house@rambler.ru>
2016-10-26 14:14:30 +0300

    start work with pagedMap

--- core/src/com/darkhouse/gdefence/Screens/CampainMap.java
+++ core/src/com/darkhouse/gdefence/Screens/CampainMap.java
@@ -2,6 +2,7 @@ package com.darkhouse.gdefence.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.darkhouse.gdefence.GDefence;
 import com.darkhouse.gdefence.Model.LevelButton;
 import com.darkhouse.gdefence.Model.Panels.GemPanel;
@@ -10,6 +11,80 @@ import com.darkhouse.gdefence.Model.Panels.UserPanel;
 import com.darkhouse.gdefence.Screens.BottomPanel.BottomPanel;
 
 public class CampainMap extends AbstractCampainScreen {
+    private class PagedMap{
+        private Page[] pages;
+        private int currentPage;
+        public boolean isPrevous(){
+            if(currentPage == 1) return false;
+            //if locked prevous
+            return true;
+        }
+
+        public boolean isNext(){
+            if(pages.length == currentPage) return false;
+            if(pages[currentPage + 1].isLocked) return false;
+            return true;
+        }
+
+        public PagedMap(int number) {
+            pages = new Page[number];
+            pages[0] = new Page(5);
+            pages[1] = new Page(6);
+            pages[2] = new Page(7);
+
+
+            currentPage = 0;
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+    private class Page extends WidgetGroup{
+        private boolean isLocked;
+
+        private LevelButton[] levels;
+
+        public Page(int buttons) {
+            levels = new LevelButton[buttons];
+            int borderSize = Gdx.graphics.getWidth()/4;
+            int sizeBetween = Gdx.graphics.getWidth()/42;
+            int levelButtonsSize[] = new int[2];
+            levelButtonsSize[0] = (Gdx.graphics.getWidth() - (borderSize * 2 + sizeBetween * (buttons - 1))) / buttons;
+            levelButtonsSize[1] = levelButtonsSize[0];
+
+            for (int i = 0; i < buttons; i++){
+                levels[i] = new LevelButton(i + 1);
+                if(!GDefence.getInstance().user.getLevelAvailable(i)){
+                    levels[i].lock();
+                }
+                levels[i].setSize(levelButtonsSize[0], levelButtonsSize[1]);
+                levels[i].setPosition(borderSize + (levelButtonsSize[0] + sizeBetween)*i, Gdx.graphics.getHeight()/2 - Gdx.graphics.getHeight()/8);
+
+                stage.addActor(levels[i]);
+            }
+
+            updateLock();
+        }
+        public void updateLock(){
+            isLocked = levels[levels.length - 1].isLocked;
+        }
+
+
+
+
+    }
+    private PagedMap pagedMap;
+
+
     //private GDefence mainClass;
 
     //private SpriteBatch batch;
@@ -34,29 +109,13 @@ public class CampainMap extends AbstractCampainScreen {
     }
 
     private void loadButtons(){
+        pagedMap = new PagedMap(3);//
+
 
         //Viewport viewport = new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         //stage = new Stage(viewport,batch);
         //Gdx.input.setInputProcessor(stage);
-        int buttons = 5;
 
-        final LevelButton[] levels = new LevelButton[5];
-        int borderSize = Gdx.graphics.getWidth()/4;
-        int sizeBetween = Gdx.graphics.getWidth()/42;
-        int levelButtonsSize[] = new int[2];
-        levelButtonsSize[0] = (Gdx.graphics.getWidth() - (borderSize * 2 + sizeBetween * (buttons - 1))) / buttons;
-        levelButtonsSize[1] = levelButtonsSize[0];
-
-        for (int i = 0; i < 5; i++){
-            levels[i] = new LevelButton(i + 1);
-            if(!GDefence.getInstance().user.getLevelAvailable(i)){
-                levels[i].lock();
-            }
-            levels[i].setSize(levelButtonsSize[0], levelButtonsSize[1]);
-            levels[i].setPosition(borderSize + (levelButtonsSize[0] + sizeBetween)*i, Gdx.graphics.getHeight()/2 - Gdx.graphics.getHeight()/8);
-
-            stage.addActor(levels[i]);
-        }
         int userPanelSize[] = {240, 120};
         int goldPanelSize[] = {60, userPanelSize[1]};
         stage.addActor(new GoldPanel(Gdx.graphics.getWidth() - userPanelSize[0] - goldPanelSize[0], Gdx.graphics.getHeight() - userPanelSize[1] -
